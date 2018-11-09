d628526cf8fa54aecbabb09e8d18345abd90ce5e
Milja V <mmcvirtanen@gmail.com>
2018-11-09 11:50:18 +0100

    Added field for sorted positions list

--- Labb2/SortedPosList.cs
+++ Labb2/SortedPosList.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Labb2
 {
     public class SortedPosList
     {
+        List<Position> positionsList = new List<Position>();
+
         public SortedPosList()
         {
         }
