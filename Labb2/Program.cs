4ff0f3641be13092e544079671625667e48ab9ea
Eric Groseclos <ericgroseclos@hotmail.com>
2018-11-12 10:10:51 +0100

    Added test code in the Main method of class Program. This will be used to test all G level functionality.

--- Labb2/Program.cs
+++ Labb2/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Labb2
 {
@@ -6,10 +10,32 @@ namespace Labb2
     {
         static void Main(string[] args)
         {
-            Position position1 = new Position(1, 9);
-            Position position2 = new Position(2, 7);
+            /*Console.WriteLine(new Position(2, 4) + new Position(1, 2) + "\n");
+            Console.WriteLine(new Position(2, 4) - new Position(1, 2) + "\n");
+            Console.WriteLine(new Position(1, 2) - new Position(3, 6) + "\n");
+            Console.WriteLine(new Position(3, 5) % new Position(1, 3) + "\n");
 
-            Console.WriteLine(position1.Equals(position2));
+            SortedPosList list1 = new SortedPosList();
+            SortedPosList list2 = new SortedPosList();
+            list1.Add(new Position(3, 7));
+            list1.Add(new Position(1, 4));
+            list1.Add(new Position(2, 6));
+            list1.Add(new Position(2, 3));
+            Console.WriteLine(list1 + "\n");
+            list1.Remove(new Position(2, 6));
+            Console.WriteLine(list1 + "\n");
+
+            list2.Add(new Position(3, 7));
+            list2.Add(new Position(1, 2));
+            list2.Add(new Position(3, 6));
+            list2.Add(new Position(2, 3));
+            Console.WriteLine((list2 + list1) + "\n");
+
+            SortedPosList circleList = new SortedPosList();
+            circleList.Add(new Position(1, 1));
+            circleList.Add(new Position(2, 2));
+            circleList.Add(new Position(3, 3));
+            Console.WriteLine(circleList.CircleContent(new Position(5, 5), 4) + "\n");*/
         }
     }
 }
